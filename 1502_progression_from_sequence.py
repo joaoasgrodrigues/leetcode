3547d1a264df3170d4beafa3253c6ba0d8c37aeb
joaoasgrodrigues <joaoasgrodrigues@gmail.com>
2024-07-01 11:03:09 +0100

    faster

--- 1502_progression_from_sequence.py
+++ 1502_progression_from_sequence.py
@@ -3,9 +3,18 @@ class Solution:
     A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
     Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false."""
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
-        arr = sorted(arr)
+        
+        arr.sort()                                              # faster in-line
         progression = arr[1] - arr[0]
-        for index, number in enumerate(arr[:-1]):
-            if arr[index + 1] - number != progression: 
+        for index in range(1, len(arr[:-1])):                   # less readable but faster
+            if arr[index + 1] - arr[index] != progression:
                 return False 
-        return True
+                
+        return True 
+    
+        # arr = sorted(arr)
+        # progression = arr[1] - arr[0]
+        # for index, number in enumerate(arr[:-1]):
+        #     if arr[index + 1] - number != progression: 
+        #         return False 
+        # return True
